e61dbc9e030944cd9eb1c1c445a303a56cb6a5d4
Joe Nagel <jnagel88@gmail.com>
2018-04-11 15:22:07 -0500

    adds 'changeDeadline' function and connects it to the 'submit' button.

--- src/app.jsx
+++ src/app.jsx
@@ -9,6 +9,10 @@ class App extends Component {
 		}
 	}
 
+	changeDeadline() {
+		this.setState({deadline: "November 25, 2018"})
+	}
+
 	render() {
 		return (
 			<div className="App">
@@ -24,7 +28,9 @@ class App extends Component {
 
 				<div>
 					<input placeholder='New Date'/>
-					<button>Submit</button>
+					<button onClick={() => this.changeDeadline()}>
+						Submit
+					</button>
 				</div>
 			</div>
 		)
