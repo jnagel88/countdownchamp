36ee0c2cee99413648ab89345bb4027684205825
Joe Nagel <jnagel88@gmail.com>
2018-04-11 15:10:39 -0500

    adds stylesheet and style classes to App component

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
 	render() {
 		return (
-			<div>Countdown Champ, App Component</div>
+			<div className="App">
+				<div className="App-Title">Countdown to Christmas 2018</div>
+				<div>
+					<div className="clock-days">14 Days</div>
+					<div className="clock-hours">30 Hours</div>
+					<div className="clock-minutes">15 Minutes</div>
+					<div className="clock-seconds">20 Seconds</div>
+				</div>
+
+				<div>
+					<input placeholder='New Date'/>
+					<button>Submit</button>
+				</div>
+			</div>
 		)
 	}
 }
